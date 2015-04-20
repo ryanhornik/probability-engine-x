2c756f16d15d795a7ece45f24e24aad7bc60ade9
TDICK54312 <gteldirith543@gmail.com>
2015-04-19 21:51:29 -0500

    Created ManualEntry Form and got basic functionality working such as clearing and going back to mainmenu. Working on the Enter button currently.

--- ManualEntry.Designer.cs
+++ ManualEntry.Designer.cs
@@ -28,80 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.EmployeeIDTextBox = new System.Windows.Forms.TextBox();
+            this.TotalSalaryTextBox = new System.Windows.Forms.TextBox();
+            this.DepartmentIDTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.JobTitleComboBox = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.OutputTextBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // EnterButton
             // 
-            this.button1.Location = new System.Drawing.Point(98, 340);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(147, 39);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Enter";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.EnterButton.Location = new System.Drawing.Point(98, 340);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(147, 39);
+            this.EnterButton.TabIndex = 0;
+            this.EnterButton.Text = "Enter";
+            this.EnterButton.UseVisualStyleBackColor = true;
+            this.EnterButton.Click += new System.EventHandler(this.EnterButton_Click);
             // 
-            // button2
+            // CancelButton
             // 
-            this.button2.Location = new System.Drawing.Point(526, 340);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 39);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
+            this.CancelButton.Location = new System.Drawing.Point(526, 340);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(150, 39);
+            this.CancelButton.TabIndex = 1;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
             // 
-            // button3
+            // ClearButton
             // 
-            this.button3.Location = new System.Drawing.Point(317, 340);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(136, 39);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Clear";
-            this.button3.UseVisualStyleBackColor = true;
+            this.ClearButton.Location = new System.Drawing.Point(317, 340);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(136, 39);
+            this.ClearButton.TabIndex = 2;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
             // 
-            // textBox1
+            // EmployeeIDTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(160, 61);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 20);
-            this.textBox1.TabIndex = 3;
+            this.EmployeeIDTextBox.Location = new System.Drawing.Point(143, 31);
+            this.EmployeeIDTextBox.Name = "EmployeeIDTextBox";
+            this.EmployeeIDTextBox.Size = new System.Drawing.Size(121, 20);
+            this.EmployeeIDTextBox.TabIndex = 3;
             // 
-            // textBox3
+            // TotalSalaryTextBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(161, 113);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(120, 20);
-            this.textBox3.TabIndex = 5;
+            this.TotalSalaryTextBox.Location = new System.Drawing.Point(144, 83);
+            this.TotalSalaryTextBox.Name = "TotalSalaryTextBox";
+            this.TotalSalaryTextBox.Size = new System.Drawing.Size(120, 20);
+            this.TotalSalaryTextBox.TabIndex = 5;
             // 
-            // textBox4
+            // DepartmentIDTextBox
             // 
-            this.textBox4.Location = new System.Drawing.Point(161, 139);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(120, 20);
-            this.textBox4.TabIndex = 6;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(161, 165);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(120, 20);
-            this.textBox5.TabIndex = 7;
+            this.DepartmentIDTextBox.Location = new System.Drawing.Point(144, 109);
+            this.DepartmentIDTextBox.Name = "DepartmentIDTextBox";
+            this.DepartmentIDTextBox.Size = new System.Drawing.Size(120, 20);
+            this.DepartmentIDTextBox.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 61);
+            this.label1.Location = new System.Drawing.Point(15, 31);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 8;
@@ -111,24 +110,24 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 87);
+            this.label2.Location = new System.Drawing.Point(15, 57);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(47, 13);
             this.label2.TabIndex = 9;
             this.label2.Text = "Job Title";
             // 
-            // comboBox1
+            // JobTitleComboBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(160, 86);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 10;
+            this.JobTitleComboBox.FormattingEnabled = true;
+            this.JobTitleComboBox.Location = new System.Drawing.Point(143, 56);
+            this.JobTitleComboBox.Name = "JobTitleComboBox";
+            this.JobTitleComboBox.Size = new System.Drawing.Size(121, 21);
+            this.JobTitleComboBox.TabIndex = 10;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(32, 113);
+            this.label3.Location = new System.Drawing.Point(15, 83);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(63, 13);
             this.label3.TabIndex = 11;
@@ -138,50 +137,83 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(32, 139);
+            this.label4.Location = new System.Drawing.Point(15, 109);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(76, 13);
             this.label4.TabIndex = 12;
             this.label4.Text = "Department ID";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.JobTitleComboBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.DepartmentIDTextBox);
+            this.groupBox1.Controls.Add(this.TotalSalaryTextBox);
+            this.groupBox1.Controls.Add(this.EmployeeIDTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(17, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(361, 244);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter Data";
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(38, 26);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.ReadOnly = true;
+            this.OutputTextBox.Size = new System.Drawing.Size(237, 259);
+            this.OutputTextBox.TabIndex = 14;
+            this.OutputTextBox.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.OutputTextBox);
+            this.groupBox2.Location = new System.Drawing.Point(400, 15);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(313, 314);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output Box";
             // 
             // ManualEntry
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(740, 391);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.EnterButton);
             this.Name = "ManualEntry";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "ManualEntry";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button EnterButton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.TextBox EmployeeIDTextBox;
+        private System.Windows.Forms.TextBox TotalSalaryTextBox;
+        private System.Windows.Forms.TextBox DepartmentIDTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox JobTitleComboBox;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox OutputTextBox;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
--- ManualEntry.cs
+++ ManualEntry.cs
@@ -12,14 +12,31 @@ namespace ProbabilityToExcel
 {
     public partial class ManualEntry : Form
     {
+        
         public ManualEntry()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void EnterButton_Click(object sender, EventArgs e)
         {
+            //ManuallyEnterData();
+        }
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
 
+        }
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            EmployeeIDTextBox.Clear();
+            JobTitleComboBox.Items.Clear();
+            TotalSalaryTextBox.Clear();
+            DepartmentIDTextBox.Clear();
+            OutputTextBox.Clear();
         }
 
         private void label1_Click(object sender, EventArgs e)
