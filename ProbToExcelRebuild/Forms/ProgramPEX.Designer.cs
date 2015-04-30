427467775c92fce231eb36a5028f0639599e1081
TDICK54312 <gteldirith543@gmail.com>
2015-04-30 15:56:55 -0500

    Added some test variables that are stored in a RegisterType and these values can be manipulated("a##",....., h##). I'm using ';' to split the values to stick into a string array that will be used to evaluate each line of the expression. All output is being sent to the debugger textbox maybe we need to make a outputbox. I'm not trying to make this fancy i swear...

--- ProbToExcelRebuild/Forms/ProgramPEX.Designer.cs
+++ ProbToExcelRebuild/Forms/ProgramPEX.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace ProbToExcelRebuild.Forms
+﻿using System.Runtime.Remoting.Channels;
+
+namespace ProbToExcelRebuild.Forms
 {
     partial class ProgramPEX
     {
@@ -32,19 +34,24 @@
             this.CompilerBox = new System.Windows.Forms.RichTextBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.DebuggerTabPage = new System.Windows.Forms.TabPage();
-            this.ErrorTabPage = new System.Windows.Forms.TabPage();
             this.DebugTextBox = new System.Windows.Forms.RichTextBox();
+            this.ErrorTabPage = new System.Windows.Forms.TabPage();
             this.ErrorTextBox = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.compileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.DebuggerTabPage.SuspendLayout();
             this.ErrorTabPage.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.CompilerBox);
-            this.groupBox1.Location = new System.Drawing.Point(22, 16);
+            this.groupBox1.Location = new System.Drawing.Point(21, 36);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(933, 369);
             this.groupBox1.TabIndex = 0;
@@ -66,10 +73,10 @@
             // 
             this.tabControl1.Controls.Add(this.DebuggerTabPage);
             this.tabControl1.Controls.Add(this.ErrorTabPage);
-            this.tabControl1.Location = new System.Drawing.Point(25, 391);
+            this.tabControl1.Location = new System.Drawing.Point(25, 426);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(929, 246);
+            this.tabControl1.Size = new System.Drawing.Size(929, 211);
             this.tabControl1.TabIndex = 1;
             // 
             // DebuggerTabPage
@@ -78,21 +85,10 @@
             this.DebuggerTabPage.Location = new System.Drawing.Point(4, 22);
             this.DebuggerTabPage.Name = "DebuggerTabPage";
             this.DebuggerTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.DebuggerTabPage.Size = new System.Drawing.Size(921, 220);
+            this.DebuggerTabPage.Size = new System.Drawing.Size(921, 185);
             this.DebuggerTabPage.TabIndex = 0;
             this.DebuggerTabPage.Text = "Debugger";
             this.DebuggerTabPage.UseVisualStyleBackColor = true;
-            // 
-            // ErrorTabPage
-            // 
-            this.ErrorTabPage.Controls.Add(this.ErrorTextBox);
-            this.ErrorTabPage.Location = new System.Drawing.Point(4, 22);
-            this.ErrorTabPage.Name = "ErrorTabPage";
-            this.ErrorTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.ErrorTabPage.Size = new System.Drawing.Size(921, 220);
-            this.ErrorTabPage.TabIndex = 1;
-            this.ErrorTabPage.Text = "Errors";
-            this.ErrorTabPage.UseVisualStyleBackColor = true;
             // 
             // DebugTextBox
             // 
@@ -100,9 +96,20 @@
             this.DebugTextBox.Location = new System.Drawing.Point(3, 3);
             this.DebugTextBox.Name = "DebugTextBox";
             this.DebugTextBox.ReadOnly = true;
-            this.DebugTextBox.Size = new System.Drawing.Size(915, 214);
+            this.DebugTextBox.Size = new System.Drawing.Size(915, 179);
             this.DebugTextBox.TabIndex = 0;
             this.DebugTextBox.Text = "";
+            // 
+            // ErrorTabPage
+            // 
+            this.ErrorTabPage.Controls.Add(this.ErrorTextBox);
+            this.ErrorTabPage.Location = new System.Drawing.Point(4, 22);
+            this.ErrorTabPage.Name = "ErrorTabPage";
+            this.ErrorTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.ErrorTabPage.Size = new System.Drawing.Size(921, 185);
+            this.ErrorTabPage.TabIndex = 1;
+            this.ErrorTabPage.Text = "Errors";
+            this.ErrorTabPage.UseVisualStyleBackColor = true;
             // 
             // ErrorTextBox
             // 
@@ -111,10 +118,45 @@
             this.ErrorTextBox.Location = new System.Drawing.Point(3, 3);
             this.ErrorTextBox.Name = "ErrorTextBox";
             this.ErrorTextBox.ReadOnly = true;
-            this.ErrorTextBox.Size = new System.Drawing.Size(915, 214);
+            this.ErrorTextBox.Size = new System.Drawing.Size(915, 179);
             this.ErrorTextBox.TabIndex = 0;
             this.ErrorTextBox.Text = "";
             this.ErrorTextBox.TextChanged += new System.EventHandler(this.ErrorTextBox_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.compileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(974, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            //this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.runToolStripMenuItem_Click);
+            // 
+            // compileToolStripMenuItem
+            // 
+            this.compileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runToolStripMenuItem,
+            this.stopToolStripMenuItem});
+            this.compileToolStripMenuItem.Name = "compileToolStripMenuItem";
+            this.compileToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.compileToolStripMenuItem.Text = "Compile";
+            this.compileToolStripMenuItem.Click += new System.EventHandler(this.compileToolStripMenuItem_Click);
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.runToolStripMenuItem.Text = "Run";
+            this.runToolStripMenuItem.Click += new System.EventHandler(this.runToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
             // 
             // ProgramPEX
             // 
@@ -123,13 +165,18 @@
             this.ClientSize = new System.Drawing.Size(974, 649);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "ProgramPEX";
             this.Text = "ProgramPEX";
             this.groupBox1.ResumeLayout(false);
             this.tabControl1.ResumeLayout(false);
             this.DebuggerTabPage.ResumeLayout(false);
             this.ErrorTabPage.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -142,5 +189,9 @@
         private System.Windows.Forms.RichTextBox DebugTextBox;
         private System.Windows.Forms.TabPage ErrorTabPage;
         private System.Windows.Forms.RichTextBox ErrorTextBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem compileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
     }
 }
--- ProbToExcelRebuild/Forms/ProgramPEX.cs
+++ ProbToExcelRebuild/Forms/ProgramPEX.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,7 +14,7 @@ namespace ProbToExcelRebuild.Forms
 {
     public partial class ProgramPEX : Form
     {
-        private ExpressionCompiler x = new CompiledExpression();
+        //private ExpressionCompiler x = new CompiledExpression();
         public ProgramPEX()
         {
             InitializeComponent();
@@ -24,6 +25,59 @@ namespace ProbToExcelRebuild.Forms
 
         }
 
-        
+        private void ProgramPEX_Load(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void runToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var reg = new TypeRegistry();
+            reg = LoadPreDefinedReg();
+            char[] delimitior = {';', '\n'};
+            string textboxCompiler = CompilerBox.Text.ToString();
+            string[] compileme = textboxCompiler.Split(delimitior);
+            //string[] textToCompile = Text.Split(delimitior);
+
+            foreach (string s in compileme)
+            {
+                var p = new CompiledExpression(compileme.ToString()) {TypeRegistry = reg};
+                if (p.Expression.Equals(null))
+                {
+
+                }
+                else
+                {
+                    var x = p.Eval();
+                    DebugTextBox.Text = x.ToString();
+                }
+                
+            }
+        }
+
+        private TypeRegistry LoadPreDefinedReg()
+        {
+            var reg = new TypeRegistry();
+            reg.RegisterType("a##", typeof(double));
+            reg.RegisterType("b##", typeof(double));
+            reg.RegisterType("c##", typeof(double));
+            reg.RegisterType("d##", typeof(double));
+            reg.RegisterType("e##", typeof(double));
+            reg.RegisterType("f##", typeof(double));
+            reg.RegisterType("g##", typeof(double));
+
+
+            return reg;
+        }
+
+        private void compileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
